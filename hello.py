fbc933b7aed2d409d932915d91cae8a902e1747e
vicky0700018 <vs2734514@gmail.com>
2025-11-25 20:24:19 +0530

    updated

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello, world!")
+def print_table(n, upto=10):
+	for i in range(1, upto + 1):
+		print(f"{n} x {i} = {n * i}")
+
+
+if __name__ == "__main__":
+	# Print table of 13
+	print_table(13, upto=10)
