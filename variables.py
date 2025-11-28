275681d74f7e06f0121731c25d30a292756e1919
vicky0700018 <vs2734514@gmail.com>
2025-11-28 19:54:58 +0530

    updated

---
+++ variables.py
@@ -0,0 +1,33 @@
+# my_name = "Rudra"
+
+# print(my_name)
+
+
+
+# @name = "Rudra"
+
+# print(@name)
+
+# print = "Hello, World!"
+
+# print(print)
+
+
+# x = 11
+# X = 22222
+
+# print(x)
+# print(X)
+
+# here , we can say small x and capital X are two different variables because python is case sensitive language.
+
+
+# Print("Prem")
+
+
+# Student Registration form , EMployee registration form 
+
+
+# name Or student_name
+
+# name OR employee_name
