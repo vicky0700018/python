908187362f1a0b495b969e83d64f1c1194851e2b
vicky0700018 <vs2734514@gmail.com>
2025-11-30 23:20:49 +0530

    updated

---
+++ operators/comparision_oprtr.py
@@ -0,0 +1,37 @@
+# comparision operator :-
+
+# => it is used to compare two values.
+# => it always returns boolean value ( True / False ).
+
+# Types of comparision operators :-
+# 1. Equal to operator (==) :-
+# 2. Not Equal to operator (!=) :-
+# 3. Greater than operator (>) :-
+# 4. Less than operator (<) :-
+# 5. Greater than or Equal to operator (>=) :-
+# 6. Less than or Equal to operator (<=) :-
+
+
+first_number = 10
+second_number = 20
+
+print(first_number == second_number)  # false 
+print("ouput: ", first_number == second_number)  # false
+
+print("ouput: ",first_number != second_number)  # true
+print("ouput: ",first_number > second_number)  # false
+print("ouput: ",first_number < second_number)  # true
+
+
+a = 15
+b = 15
+
+print("ouput: ",a >= b)  # true
+print("ouput: ",a <= b)  # true
+
+
+## assignment operators
+## Logical Operators 
+## bitwise operators
+## Indentity operators
+## Membership operators
