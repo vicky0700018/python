f7ab6946dcd53304e0f77e41d8adde391958219b
vicky0700018 <vs2734514@gmail.com>
2025-12-04 00:14:58 +0530

    updated

---
+++ cource_content.py
@@ -0,0 +1,69 @@
+# 1. Core Python
+# 2. OOPs Concepts
+
+# 3. SQL, Database ( MySQL, PostgreSQL , SQLite )
+# => create table, insert data, update data, delete data, fetch data.
+# => drop table, alter table. drop colm , add colm.
+# => joins ( inner join, left join, right join, full join )
+# => primary key, foreign key, unique key, not null, default value.
+# => union , intersection. 
+# => oprerators in sql.
+# => Tenant management system project using python and database.
+
+# Frontend Technologies ( HTML, CSS, JavaScript )
+# => Html 
+# => CSS
+# => JavaScript basics
+# => Bootstrap
+
+# Backend Technologies ( Django Framework )
+# => how to connect frontend with backend using Django and database.
+# => Django Framework introduction
+# => Setting up Django environment
+# => Django Project and App creation
+# => Django Models 
+# => Django Migration
+# => Django Admin Panel
+# => how to create superuser (OWNER of project)
+# => Django Views and URL 
+# => Django Templates ( HTML code )
+
+# => Django Forms
+# => Django Static files ( CSS, JavaScript, Images )
+
+# => CRUD operation using Django ( Create, Read, Update, Delete )
+# => how to store data using forms in database.
+# => how to show data from database to html page.
+# => how to update data in database using forms.
+# => how to delete data from database.
+
+# => Django session management
+# => what is session and cookies.
+
+# => 8 - 9 Projects using Django Framework and Database.
+
+# => Deyployment of Django project on server ( Rendder ,Vercel, Netlify )
+
+
+# => API using Django Rest Framework ( DRF )
+# => Postman API
+# => Swagger API
+# => Fast api 
+
+# => what is API
+# => GET method => it is used to show data from server.
+
+# => POST method => it is used to create data on server.
+
+# => PUT method => it is used to update complete data on server.
+
+# => DELETE method => it is used to delete data from server.
+
+# => patch method => it is used to update partial data on server.
+
+# # Data Science Basics
+
+
+# 11 Project 
+# => 2 Major Projects
+# => 9 Mini Projects
