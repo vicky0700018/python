65fbd6bf099e2abbc5a37590e62d13b5504b8195
vicky0700018 <vs2734514@gmail.com>
2025-12-03 19:18:07 +0530

    updated

---
+++ operators/Bitwise_Operator.py
@@ -0,0 +1,16 @@
+
+# AND (&)  => if both bits are 1 then only result is 1 otherwise 0
+# OR  (|)  => if any one bit is 1 then result is 1 otherwise 0
+# XOR (^)  => if both bits are different then only result is 1 otherwise 0
+
+
+
+# a = 10
+# b = 3
+# print("AND operator:", a & b )   
+
+
+
+a = 14
+b = 11
+print("AND operator:", a & b )
