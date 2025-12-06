5b4211633fc113cb645417dff609baab2f5e6982
vicky0700018 <vs2734514@gmail.com>
2025-12-06 20:02:17 +0530

    updated

--- testing.py
+++ testing.py
@@ -1,9 +1,33 @@
 # comparision_oprtr
-a=10
-b=20
-print(a==b)
-print(a!=b)
-print(a>b)
-print(a<b)
-print(a>=b) 
-print(a<=b)
+# a=10
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b) 
+# print(a<=b)
+a = 10
+# b = 10
+# print( a <= b)
+
+# # Arithmetic operator
+# x=30
+# # print(x%y)
+# # y=20
+# y=6
+
+# # print(x-y)
+# # print(x+y)
+# # print(x*y)
+# # print(x/y)
+# print(x//y)
+# print(x**y)
+
+# Logical Operators :-
+
+a = True
+b = False
+print( a and b )  
+print( a or b )
+print ( not a)
