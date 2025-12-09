d157d8439c9e46a987f7d35e4d182d73ac3fe300
vicky0700018 <vs2734514@gmail.com>
2025-12-09 20:03:29 +0530

    updated

---
+++ String/str_intro.py
@@ -0,0 +1,111 @@
+# my_name = "sonu"  # my_name => variable , "sonu" => value
+
+
+# string example:-
+
+# "tridev", "Ravi", "rudra"
+
+# Character example:-
+
+# 'a', 'b', 'c'
+
+
+# string :-
+
+# it is just like a sequence of characters enclosed within single or double quotes or triple quotes.
+
+# => it is collection of characters.
+
+
+
+
+# my_name = 'vicky'  # single quotation  mark
+
+# print(my_name)
+
+
+# watch = "vicky's watch"  # double quotation mark
+
+
+# sentence = '''it was fantastic topic "HOstel life" '''  # triple quotation mark
+
+
+
+# str1 = "vicky"
+
+# print(str1)
+
+
+
+# str1 = "vicky"
+
+# print("my name is => ", str1)
+
+
+# name = "vicky kumar"
+# branch = "CSE"
+
+# city = "lakhisari"
+# state = "Bihar"
+
+# pincode = "811107"
+
+
+# print("Student Name :", name ,  "Branch :", branch ,)
+
+# print("City :", city , "State :", state , "Pincode :", pincode)
+
+
+
+# input() => it is used to take data from user side. 
+
+
+std_name = input("Hii, Please enter your name: ")
+
+print("your name is => ", std_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# string : -
+# => 
+
+
+
+# Grammar :-                  coding :-
+
+# alphabets , letters         => Characters/letters
+
+
+# group of lettrs (words)     => string 
+
+# group of words ( Sentence )  => string
+
+# group of sentence ( Paragrph) => string
