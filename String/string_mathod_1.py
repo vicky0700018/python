39260678dc5d232a209e92ccc313d8ca3b6c6ae0
vicky0700018 <vs2734514@gmail.com>
2025-12-15 20:03:25 +0530

    updated

---
+++ String/string_mathod_1.py
@@ -0,0 +1,17 @@
+# std_name = "Sourabh"
+
+# print("your name is => ", std_name)
+
+# Indexing :-
+my_name = "Rahul"
+print(my_name[0])
+print(my_name[4])
+
+print(my_name[-1])
+
+           
+# # 0 1 2 3 4 
+# # R u d r a
+# my_name = "Rudra"
+
+# print(my_name[3])
