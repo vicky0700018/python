5d8c1ea0936ae3d592a22fe9ebba208fc63730d5
vicky0700018 <vs2734514@gmail.com>
2025-12-20 20:01:13 +0530

    updated

---
+++ String/string_slicing.py
@@ -0,0 +1,90 @@
+# String SLicing ( two Parametrs):- 
+
+
+# my_name = "Tridev"  # we have to print only "rid" from "Tridev"
+
+# print(my_name[3])  # d
+# print(my_name[4])  # e
+# print(my_name[0])  # T
+
+
+
+# String indexing :-
+
+# 0 1 2 3 4 5
+# T r i d e v
+
+
+# my_name = "Tridev"  # we have to print only "rid" from "Tridev"
+
+# print(my_name[1:4])
+
+# starting point : 1
+# ending point : 4  (Excluded)
+
+# -------------------------------------------------- 
+
+# 0 1 2 3 4 5
+# P Y T H O N
+
+# language = "PYTHON" # we have to print "HO" from "PYTHON"
+
+# print(language[3:5]) # HO
+
+# starting point : 3
+# ending point : 5 ( EXCLUDED )
+
+
+# Rules:- 
+
+# 1. By default starting point will be always 0 .
+# 2. by default ending point will be last index . 
+
+# ------------------------------------------ 
+
+# 0 1 2 3 4 5 
+# V i k r a m
+
+# name = "Vikram"
+# print(name[ : 3])  # 0 - 2  => Vik
+
+# print(name[2: ])  # kram
+
+# starting point : 2
+# ending point : last index
+
+
+# --------------------------------------------------- 
+
+
+# 0 1 2 3 4 5 
+# C a n a d a
+
+# country = "Canada"
+
+# print(country[0:5]) # Canad
+# print(country[ : 5]) # Canad
+# print(country[ : ])  # Canada
+ 
+#  ------------------------------------------------ 
+
+
+
+
+# 0 1 2 3 4 5 
+# C a n a d a
+
+# country = "Canada" 
+ 
+
+# print(country[2:5])                  # we have to print "nad"
+
+# print(country[0 : 3])                # we have to print "Can"
+
+
+
+# String SLicing ( Three  Parametrs):- 
+
+# starting point 
+# ending point 
+# GAP ( N - 1 )
