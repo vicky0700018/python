434a85cff4c4ca9d95d63a6a1c6e3d402c651166
vicky0700018 <vs2734514@gmail.com>
2025-12-25 22:39:09 +0530

    updated

---
+++ LIST/list.py
@@ -0,0 +1,16 @@
+# list => it can store multiple type of data.
+
+
+# my_list = [ 11, 12, 23, 33]
+
+# list2 = [13, 13.3 , "Sushil", "Rudra", 5, True]
+
+
+
+# integer  => 11, 12, 23, 33
+
+# float => 22.3, 44.9, 4.8
+
+# string => "Tridev", "this is Rudra"
+
+# boolean => True, False
