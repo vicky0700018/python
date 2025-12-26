fe767fa172e5af2d597e13714a67f4d08ac5b60a
vicky0700018 <vs2734514@gmail.com>
2025-12-26 19:46:01 +0530

    updated

---
+++ LOOP/for_loop.py
@@ -0,0 +1,44 @@
+# for i in range(0,5):
+#                     print(i)
+
+# ------------------------------------------------- 
+
+# for i in range(5):
+#                     print(i)
+
+
+
+# ----------------------------------------- 
+
+
+# for tridev in range(0,5):
+#                     print(tridev)
+
+
+# for i in range(0,5):
+#                     print(i)
+
+
+# i => loop variable 
+# starting point => 0 
+# ending point => 5 ( excluded )
+
+# ------------------------------------------ 
+
+# for i in range(5):
+#                     print(i)
+
+# by default starting point will be 0 
+# starting point => 0
+# ending point => 5 ( excluded )
+
+# -----------------------------------------------
+
+for i in range(5 ,16):
+                    print(i) # 5 - 15 
+
+# starting point => 5
+# ending point => 16 (excluded )
+# i => loop variable 
+
+
