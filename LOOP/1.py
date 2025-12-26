9e9ff5673a24f8182b9da8adf218d3db50cdbf03
vicky0700018 <vs2734514@gmail.com>
2025-12-26 19:42:40 +0530

    updated

---
+++ LOOP/1.py
@@ -0,0 +1,35 @@
+# # print("Rudra")
+# # print("Rudra")
+# # print("Rudra")
+# # print("Rudra")
+# # print("Rudra")
+# # print("Rudra")
+# # print("Rudra")
+# # print("Rudra")
+# # print("Rudra")
+# # print("Rudra")
+
+# # Loop => it is used to perform any repeted task.
+
+
+
+# for i in range(10):
+#                     print("Rudra")
+
+# Types of Loop :-
+
+# 1. for loop 
+# 2. while loop 
+
+# for loop :- 
+
+
+
+
+# for loop => 
+
+# 1. check condition => true 
+
+# 2. execute some lines of code 
+
+# 3. i ++
