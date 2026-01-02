30fc2b388868f9a4206b3b8089b15570b26affc1
vicky0700018 <vs2734514@gmail.com>
2026-01-03 03:04:55 +0530

    updated

--- prac.py
+++ prac.py
@@ -1,17 +1,21 @@
-# a=10
-a=1.1
-a= complex(8+2j)
-b="vicky"
+# # a=10
+# a=1.1
+# a= complex(8+2j)
+# b="vicky"
 
-c=True
-d=None
-# b=10 
-# b=20
-print(a,b)
+# c=True
+# d=None
+# # b=10 
+# # b=20
+# print(a,b)
 
-print(id(a),id(b))
+# print(id(a),id(b))
 
-print(f"type of a is {type(a)}")
-print (f"type of b is {type(b)}")
-print (f"type of c is {type(c)}")
-print (f"type of d is {type(d)}")
+# print(f"type of a is {type(a)}")
+# print (f"type of b is {type(b)}")
+# print (f"type of c is {type(c)}")
+# print (f"type of d is {type(d)}") 
+
+
+dict1={"name":"pallavi","age":24,"city":"bihar","phone":1234567890,"email":"vs@gmail.com"}
+print(dict1)
