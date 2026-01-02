f1db4f240e09da99161b3bde20b464fd1da611c0
vicky0700018 <vs2734514@gmail.com>
2026-01-03 02:47:19 +0530

    updated

---
+++ prac.py
@@ -0,0 +1,14 @@
+a=10
+# b=10 
+b="vicky"
+# b=20
+c=True
+d=None
+print(a,b)
+
+print(id(a),id(b))
+
+print(f"type of a is {type(a)}")
+print (f"type of b is {type(b)}")
+print (f"type of c is {type(c)}")
+print (f"type of d is {type(d)}")
