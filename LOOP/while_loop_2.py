8c6a5b06902ecc441172e60e237e9188b8122409
vicky0700018 <vs2734514@gmail.com>
2026-01-11 20:50:29 +0530

    updated

---
+++ LOOP/while_loop_2.py
@@ -0,0 +1,17 @@
+# while loop 
+
+
+# i++ OR i = i+1 OR  i += 1 => these are same thing
+
+# i = 0
+
+# while(i<5):
+#                     print(i)
+#                     i = i + 1
+
+
+i = 0
+
+while(i<5):
+                    print(i)
+                    i += 1
