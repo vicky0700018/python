9c8bae4bf7cdf389b92ceb12330b0dfd81b4a633
vicky0700018 <vs2734514@gmail.com>
2026-01-12 20:46:25 +0530

    updated

---
+++ LOOP/imput_function.py
@@ -0,0 +1,52 @@
+# a = 10 
+# b = 20 
+# print(a + b)  # 30 
+
+
+input() => it is used to take data from the user side .
+=> it is similar like scanf which is used in C language. 
+=> By default it gives us String value. 
+=> that's why we have to specify data type whatever we want in result.
+
+
+scanf => it is used to take data from user. 
+
+
+
+# a = 10 
+# b = 20 
+# print(a + b)  # 30 
+
+
+
+# first_name = input("HII guys! please enter your first name: ")
+# last_name = input("Hii guys! please enter your last name: ")
+
+# print(first_name, " ", last_name)
+
+# Rohitxyz  Rohit xyz
+
+# # Data type : - 
+# int => 22,344,44
+# float => 33.4, 5.6
+# String => "Tridev", "Amit", 'A', "THis is Sourabh"
+# boolean => True , False
+
+
+# name = input("hii! please enter your name: ")
+# age = int(input("hii! please enter your age:"))
+
+# print(name, "", age)
+
+
+
+# name = input("hii! please enter your name: ")
+# age = int(input("hii! please enter your age:"))
+
+# print(name, "", age)
+
+# print("your name is : ", name )
+# print("your age is : ", age)
+
+
+# print("rohit", "Tridev")
