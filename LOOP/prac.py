f59121204ba931aff64f4b586dd02a6533974efc
vicky0700018 <vs2734514@gmail.com>
2026-01-12 20:42:17 +0530

    updated

---
+++ LOOP/prac.py
@@ -0,0 +1,63 @@
+# for i in range(4):
+#                     print(i) # 0,1,2,3
+
+# i => loop variable 
+# starting point => 0
+# ending point => 4 (excluded)
+# gap = 1
+# Actual Range( 0 - 3)
+
+# output => 0,1,2,3
+
+
+
+# for i range(8,12):
+#                     print(i)
+
+# i => loop variable 
+# starting point => 8
+# ending point => 12 ( Excluded)
+# gap = 1
+# Actual range( 8 - 11)
+# Output => 8,9,10,11
+
+
+
+# for i in range(1,10,2):
+#                     print(i)
+
+# i => loop varaible 
+# starting point => 1
+# ending point => 10 (excluded)
+# gap => 2 => n-1 => 2-1 => 1 , that mean we have to skip one element.
+
+# Acutal range( 1 - 9 )
+
+# ouptut => 1, 3, 5, 7, 9
+
+
+
+# write a program to print the table of 11. 
+
+# for i in range(11,111,11):
+#                     print(i)
+
+# i => loop varaible 
+# starting point => 11
+# ending point => 111 (excluded)
+# gap => 11 => n-1 => 11-1 => 10 , that mean we have to skip 10 elements.
+
+# Acutal range( 11 - 110 )
+
+# ouptut => 11,22, 33..........
+
+
+
+# starting point => 0
+# ending point ( Excluded)
+# Gap => N-1 ( by default gap = 1 )
+
+
+# NOte: 
+# 1. by default starting point will be 0 
+# 2. By default Gap will be 1
