51a86dd6f90625b0f16b5f7a0e95afefe0c867a9
vicky0700018 <vs2734514@gmail.com>
2026-01-21 22:19:01 +0530

    updated

---
+++ TUPLE/tuple.py
@@ -0,0 +1,65 @@
+# Tuple :- 
+
+# => it can store any type of data similar as List .
+# => it is Immutable 
+
+
+# tupleee = (22,33,44,55,"Rudra", 6.7)
+
+# print(tupleee)
+
+
+# indexing => positive , NEgative indexing 
+
+# tupleee = (22,33,44,55,"Rudra", 6.7)
+
+# print(tupleee)
+
+# print(tupleee[2])
+# print(tupleee[4])
+
+
+
+
+# tupleee = (22,33,44,55,"Rudra", 6.7)
+
+# print(tupleee)
+
+# for i in tupleee:
+#                     print(i)
+
+
+
+# tuple methods 
+
+# In Python, tuple has only two methods.
+
+# 1. count 
+# 2. index 
+
+
+# tupleee = (22,33,44,55,"Rudra", 6.7, "Rudra")
+
+# print(tupleee.count("Rudra"))
+
+
+
+tupleee = (22,33,44,55,"Rudra", 6.7, "Rudra")
+
+print(tupleee.index("Rudra"))
+
+
+
+
+# 1.how to apply for loop on list , string and tuple.
+# 2. Mutable and immutable 
+# 3. what is Tuple . and indexing in tuple and it's methods. 
+
+
+
+# 1. list is mutable , prove it . 
+# 2. tuple is immutable , prove it. 
+# 3. what is mutable and immutable . 
+# 4. how to apply for loop in your name 
+# 5. how to apply for loop in any list . 
+# 6. how to apply for loop in any tuple .
