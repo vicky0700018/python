7ac864c4f8de7b8c1e9f8daf33e221ebc29a2e5b
vicky0700018 <vs2734514@gmail.com>
2026-01-21 22:19:26 +0530

    updated

---
+++ conditional_statement/for_list_all_apply.py
@@ -0,0 +1,53 @@
+# for i in range(5):
+#                     print(i)
+
+# i => loop variable 
+
+# # 0 ,1,2,3,4
+
+
+# how to apply for loop in a list 
+
+#          0   1  2  3  4  5
+# my_list = [22,33,44,55,66,67]
+
+# for i in my_list:
+#                     print(i)
+
+# i = loop variable 
+
+
+
+
+
+# for i in range(5):
+#                     print(i)
+
+# i => loop variable 
+
+# 0 ,1,2,3,4
+
+
+
+#               0        1     2  3  4
+# my_list = ["Sachin", "Rohan", 33,44,55]
+
+# for i in my_list:
+#                     print(i)
+
+
+# how to apply for loop in any String 
+
+# my_word = "Programming"
+
+# for i in my_word:
+#                     print(i)
+
+
+
+# my_sentence = "AYush is a good guy"
+
+# for i in my_sentence:
+#                     print(i)
+
+# print(my_sentence)
