ab8f764b119f7db2f1fb8c5a0419fd46d46e17fd
vicky0700018 <vs2734514@gmail.com>
2026-01-22 21:13:18 +0530

    updated

---
+++ revesion.py
@@ -0,0 +1,40 @@
+#  1. list is mutable , prove it . 
+# my_list = [10, 20, 30, 40, 50, 60]
+# my_list[0] = 99
+# my_list[3] = 88
+# print(my_list)
+# # output: [99, 20, 30, 88, 50, 60]
+
+
+# # 2. tuple is immutable , prove it. 
+# my_tupleee = ("Vicky", "Rahul", "Aman")
+# my_tupleee[0] = "Ravi"
+# print(my_tupleee)
+
+#output: TypeError: 'tuple' object does not support item assignment
+
+# 3. what is mutable and immutable . 
+# Mutable objects are those whose state or content can be modified after they are created.
+# Changes made to a mutable object affect the same memory location.
+# Common examples of mutable objects are list, dictionary, and set.
+
+# immutable :- Immutable objects ki value creation ke baad change nahi ki ja sakti hai.
+# Same object ke andar data modify nahi hota hai (naya object banta hai).
+# Examples: string, integer, tuple, frozenset.
+
+# 4. how to apply for loop in your name 
+# my_name = "vicky"
+# for i in my_name:
+#                  print(i)
+
+
+# # 5. how to apply for loop in any list .
+# my_list = [10, 20, 30, 40, 50]
+# for i in my_list:
+#     print(i)
+
+
+# 6. how to apply for loop in any tuple .
+# my_tuple = ("Vicky", "Rahul", "Aman")
+# for i in my_tuple:
+#     print(i)
