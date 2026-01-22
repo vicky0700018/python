9fbd1674323adb5bcc8159f919acf3c42a409149
vicky0700018 <vs2734514@gmail.com>
2026-01-22 21:13:50 +0530

    updated

---
+++ mutable_Immutable/mutable_Immutable.py
@@ -0,0 +1,56 @@
+# list => add , delete , 
+
+# string => add or delete 
+
+
+
+# Mutable :-
+# => that means changable 
+# => it can change specifically as well as Entirely . 
+# Example : List, dictionary. 
+
+
+# Example 
+
+# my_list = [11,22,33,44]
+# my_list.pop()
+# print(my_list)
+
+# ----------------------------------------------------- 
+# my_list = [11,22,33,44]
+# print(my_list)
+
+# my_list[2] = 3333
+
+# print(my_list)
+
+
+
+
+# Immutable :-
+# => that means not changeable 
+# => it can change only Entirely . 
+# => it can't change specifically .
+# Example: String, tuple 
+
+
+# my_name = "Ayush"
+
+# print(my_name[2])  # u 
+
+# my_name[2] = "o"
+
+# print(my_name)
+
+
+
+# ---------------------------- 
+
+
+# my_name = "Ayush"
+
+# print(my_name)
+
+# my_name = "Vicky"
+
+# print(my_name)
