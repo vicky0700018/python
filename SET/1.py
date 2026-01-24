ec6dbbfc294e65cc42cb7faf394ca2f069a06818
vicky0700018 <vs2734514@gmail.com>
2026-01-24 22:32:48 +0530

    updated

---
+++ SET/1.py
@@ -0,0 +1,98 @@
+# Set :- 
+
+
+# list = [ 234,45,55]
+# tuplee = (7,33,44,5)
+
+# set = { 12,33,44,55}
+
+# => it can not contain any duplicate element. 
+# => it can contain only unique elements.
+# # => A set is an unordered collection of Unique elements .
+# => it does not follow indexing. 
+# => it can also contain any type of data similar as List. 
+# it is mutable 
+
+
+
+#          0   1  2  3  4
+# my_set = { 11,22,33,33,33,55, 66, 88,99,99} # here, 33 is Duplicate/Repeated element. 
+# print(my_set)
+
+
+
+#          0   1  2  3  4
+# my_set = { 11,22,33,33,33,55, 66, 88,99,"Rudra"}  
+# print(my_set)
+
+
+# methods 
+
+# 1. add => used to add any single  element .
+# 2. update => used to add multiple element.
+# 3. remove => used to remove any element, if element is not available , it gives erorr
+# 4. discard => used to remove any elemnt, if element is not available , it doesn't give error 
+
+# 5. pop => used to delete any random elemnt. 
+# 6. clear
+# 7. 
+
+
+# my_set = {11,22,34,55}
+
+# print("before performing operation", my_set)
+
+# my_set.add(555)
+# print("after performing any operation", my_set)
+
+
+
+# my_set = {11,22,34,55}
+
+# print("before performing operation", my_set)
+
+# my_set.update([2222,3333])
+# print("after performing any operation", my_set)
+
+
+
+
+# my_set = {11,22,34,55}
+
+# print("before performing operation", my_set)
+
+# my_set.remove(222) # if it does not find the element, it gives error
+# print("after performing any operation", my_set)
+
+
+
+
+# my_set = {11,22,34,55}
+# print("before performing operation", my_set)
+
+# my_set.discard(222) 
+# print("after performing any operation", my_set)
+
+
+
+
+# my_set = {11,22,343,55,5,6,9,99,101,34}
+# print("before performing operation", my_set)
+
+# my_set.pop() 
+# print("after performing any operation", my_set)
+
+
+
+
+# my_set = {11,22,343,55,5,6,9,99,101,34}
+# print("before performing operation", my_set)
+
+# my_set.clear() 
+# print("after performing any operation", my_set)
+
+
+
+# intersection, union , copy 
+
+# send me example
