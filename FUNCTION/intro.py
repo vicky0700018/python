d3d67164e6a87a9840a8bcf8785988ab715588b2
vicky0700018 <vs2734514@gmail.com>
2026-01-26 17:18:13 +0530

    updated

---
+++ FUNCTION/intro.py
@@ -0,0 +1,125 @@
+# Function :-
+
+# => it is just like of block of code . that saves our time , and increase code reusability and code readablity.
+
+# 1. it saves our time .
+# 2. it increases code reusability. 
+# 3. it increses the code readablity . 
+
+
+# function definition 
+
+# def print_my_name():
+#                     print("Tridev SHarma")
+
+
+# function call 
+# print_my_name()
+
+# print_my_name()
+
+# print_my_name()
+
+# -------------------------------------------- 
+
+
+# here we haved defined our function 
+# def print_all_students_name():
+#                     print("Tridev")
+#                     print("VIcky")
+#                     print("Rudra")
+#                     print("NEha")
+#                     print("SUshil")
+#                     print("SOurabh")
+
+# # here , we call our fxn 
+# print_all_students_name()
+
+# print("hii guys! please print the students again")
+
+
+
+# print_all_students_name()
+
+
+
+
+# ---------------------------------------------------- 
+
+
+
+# here we haved defined our function 
+def print_all_students_name():
+                    print("Tridev")
+                    print("VIcky")
+                    print("Rudra")
+                    print("NEha")
+                    print("SUshil")
+                    print("SOurabh")
+
+# here , we call our fxn 
+print_all_students_name()
+
+print("hii guys! please print the students again")
+
+print_all_students_name()
+
+a = 10 
+b = 20 
+
+if a > b :
+                    print("a is greater")
+else:
+                    print("b is greater")
+
+
+print_all_students_name()
+
+print("thik hai yrr bhot ho gya")
+
+print_all_students_name()
+
+print("thik hai it's last time to print std name ")
+
+print_all_students_name()
+
+
+
+
+
+
+
+
+
+
+
+# function 
+def tridev():
+            print("Name Tridev")
+            print("course BCA")
+            print("Age 18")
+tridev()
+
+
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+
+def info(name, age):
+    return f"Name: {name}, Age: {age}"
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+add_result = add(a, b)
+print(f"Addition: {add_result}")
+
+sub_result = subtract(a, b)
+print(f"Subtraction: {sub_result}")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(info(name, age))
+
+
