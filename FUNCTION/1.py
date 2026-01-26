3a62c59697f9019b20bf5ff2564af43764034b07
vicky0700018 <vs2734514@gmail.com>
2026-01-26 17:18:46 +0530

    updated

---
+++ FUNCTION/1.py
@@ -0,0 +1,40 @@
+# Function has two parts : 
+# 1. Function Definition
+# 2. Function Call 
+
+
+
+# task: how to create a function 
+
+# def fxn_name():
+#                     some lines of code 
+# -------------------------------------------------------- 
+
+# def print_tridev_info():
+#                     print("Tridev")
+#                     print("BCA")
+#                     print("Sobhit University")
+
+
+# print_tridev_info()
+
+
+# ----------------------------------------------- 
+
+
+# function definition 
+def print_tridev_info():
+                    print("Tridev")
+                    print("BCA")
+                    print("Sobhit University")
+
+# def => keyword to declare a function in Python 
+# print_tridev_info => this is Function name 
+
+
+# print("Tridev")                     => 
+# print("BCA")                        => task of this function 
+# print("Sobhit University")          => 
+
+# function call 
+print_tridev_info()
