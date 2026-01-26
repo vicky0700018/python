a207217ed3fe2f5e63038517209bbe38e1dd5174
vicky0700018 <vs2734514@gmail.com>
2026-01-26 17:19:40 +0530

    updated

---
+++ FUNCTION/2.py
@@ -0,0 +1,48 @@
+# Types of Function :- 
+
+
+# 1. function with no parameter 
+# 2. Function with Parameter 
+#    a. function with positional arguments 
+#    b. function with Default arguments 
+
+
+
+# a = 10 
+# b = 20 
+# print(a+b)
+
+# ----------------------------------------------------------- 
+# a = int(input("enter the value of a : "))
+# b = int(input("enter the value of b : "))
+
+# print("the sum of a and b is : ", a + b )
+
+# ------------------------------------------------------------- 
+
+
+# FUnction with parameter 
+
+
+# def sum_of_two_number(a,b):
+#                     print("the sum of a and b is : ", a + b)
+
+
+# sum_of_two_number(20,30)
+# sum_of_two_number(10,90)
+# sum_of_two_number(33,3)
+# sum_of_two_number(44,6)
+# sum_of_two_number(19999,99)
+
+
+# -------------------------------------------------------------- 
+
+
+def sum_of_three_numbers(a,b,c):
+                    print("the sum of three is : ", a + b + c)
+
+
+sum_of_three_numbers(2,4,99)
+sum_of_three_numbers(99,33,20000)
+
+sum_of_three_numbers(44,55,999)
