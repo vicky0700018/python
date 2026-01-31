ab353175e989176a1228004e8b6931e91b54fe7a
vicky0700018 <vs2734514@gmail.com>
2026-01-31 22:02:14 +0530

    updated

---
+++ FUNCTION/3.py
@@ -0,0 +1,16 @@
+# function with positional arguments 
+
+# def sum_of_two_nums(first_num, second_num):
+#                     print(first_num + second_num)
+
+# sum_of_two_nums(10,20)
+
+# --------------------------------------------------- 
+# function with default parameters 
+
+
+def sum_of_two_nums(first_num, second_num = 30):
+                    print(first_num + second_num)
+
+sum_of_two_nums(10)
+sum_of_two_nums(10,500)
