61cf3bfcc8b4188fcc5b4f3ff99d4733b71b6bf1
vicky0700018 <vs2734514@gmail.com>
2026-01-31 22:03:00 +0530

    updated

---
+++ OOPS/2.py
@@ -0,0 +1,41 @@
+# class Room:
+#                     num_of_students = 80 
+#                     num_of_teachers = 2
+#                     no_of_desks = 50
+#                     blackboard = 2
+# print(num_of_teachers) # it give error
+
+# ---------------------------------------------------------- 
+
+# here , Room is the class 
+# class Room:
+#                     num_of_students = 80 
+#                     num_of_teachers = 2
+#                     no_of_desks = 50
+#                     blackboard = 2
+
+# my_obj = Room()  # here, my_obj is the object of Room class. 
+
+# print(my_obj.num_of_students)
+# print(my_obj.num_of_teachers) # 2
+# print(my_obj.no_of_desks) # 50
+# print(my_obj.blackboard) # 2
+
+
+# what is class ? 
+# # => class is just like a blueprint or sample, that contains some properties and features or behaviour
+
+
+# what is object ? 
+# => by using this , we can access all properties and features of a class . 
+
+
+class vicky:
+                    degree = "BCA"
+                    address = "xyz"
+                    email = "vicky@gmail.com"
+                    age = 20 
+
+vicky_obj = vicky()  # here, vicky_obj is a object of vicky class 
+
+print(vicky_obj.degree, vicky_obj.age)
